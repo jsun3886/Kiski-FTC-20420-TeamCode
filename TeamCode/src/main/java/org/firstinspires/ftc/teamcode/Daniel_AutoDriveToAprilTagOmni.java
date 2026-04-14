804d1892887e0a596b901ae887968b13a73400cd
Daniel AGI <haokai.yang@kiski.org>
2026-04-14 13:46:21 -0400

    Refine drive and turn values in auto drive logic
    Adjusted driving and turning values for improved maneuverability.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Daniel_AutoDriveToAprilTagOmni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Daniel_AutoDriveToAprilTagOmni.java
@@ -124,7 +124,7 @@ public class Daniel_AutoDriveToAprilTagOmni extends LinearOpMode {
             if (time < 4.0) {
                 drive = 0.5;                    // forward
             } else if (time < 5.5) {
-                turn = 0.7;                    // turn left 90°
+                turn = 0.73;                    // turn left 90°
             } else if (time < 9.5) {
                 drive = 0.5;                    // forward
             } else if (time < 24.5) {
@@ -132,21 +132,21 @@ public class Daniel_AutoDriveToAprilTagOmni extends LinearOpMode {
                 strafe = 0;
                 turn = 0;
             }else if (time < 26) {
-                turn = -0.7;                     // turn right 90° , 11.0
+                turn = -0.73;                     // turn right 90° , 11.0
             } else if (time < 28) {
-                drive = 0.5;                    // forward , 13.0
+                drive = 0.8;                    // forward , 13.0
             } else if (time < 29.5) {
-                turn = -0.7;                     // turn right 90° , 14.5
+                turn = -0.72;                     // turn right 90° , 14.5
             } else if (time < 30.5) {
-                drive = 0.5;                    // forward, 15.5
+                drive = 0.8;                    // forward, 15.5
             } else if (time < 31.5) {
-                turn = -0.5;                     // small right turn , 16.5
+                turn = -0.52;                     // small right turn , 16.5
             } else if (time < 36.5) {
-                drive = 0.5;                    // forward , 21.5
+                drive = 0.8;                    // forward , 21.5
             } else if (time < 39.5) {
-                turn = -0.6;                     // bigger right turn , 24.5
+                turn = -0.62;                     // bigger right turn , 24.5
             } else if (time < 45) {
-                drive = 0.5;                    // forward , 30.5
+                drive = 0.8;                    // forward , 30.5
             } else {
                 drive = 0;
                 strafe = 0;
